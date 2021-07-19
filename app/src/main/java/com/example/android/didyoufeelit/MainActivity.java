29af6b98015cde18773159996b12e9b3de1814e8
adityadav23 <aditya023.ay@gmail.com>
2021-07-20 00:21:01 +0530

    added code to handle null urls

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -72,5 +72,36 @@ public class MainActivity extends AppCompatActivity {
             updateUi(result);
         }
     }
+    /**
+     * This method is invoked (or called) on a background thread, so we can perform
+     * long-running operations like making a network request.
+     *
+     * It is NOT okay to update the UI from a background thread, so we just return an
+     * {@link Event} object as the result.
+     */
+    protected Event doInBackground(URL... urls) {
+        // Don't perform the request if there are no URLs, or the first URL is null.
+        if (urls.length < 1 || urls[0] == null) {
+            return null;
+        }
 
+        Event result = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
+        return result;
+    }
+
+    /**
+     * This method is invoked on the main UI thread after the background work has been
+     * completed.
+     *
+     * It IS okay to modify the UI within this method. We take the {@link Event} object
+     * (which was returned from the doInBackground() method) and update the views on the screen.
+     */
+    protected void onPostExecute(Event result) {
+        // If there is no result, do nothing.
+        if (result == null) {
+            return;
+        }
+
+        updateUi(result);
+    }
 }
